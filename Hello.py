7a9bf4e87c12be683aa77697a74f7e0a0c2fb82c
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-05-02 18:48:15 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,17 +18,28 @@ order_book_updated=st.empty()
 order_book_updated.text(f"Orderbook : ")
 order_datatable=st.empty()
 
+def get_yf_data(symbol,interval):
+  df=yf.Ticker(symbol).history(interval=interval,period=str(2)+"d")
+  df['Datetime'] = df.index
+  df['Datetime']=df['Datetime'].dt.tz_localize(None)
+  df.index=df['Datetime']
+  df=df[['Datetime','Open','High','Low','Close','Volume']]
+  df['Date']=df['Datetime'].dt.strftime('%m/%d/%y')
+  df['Datetime'] = pd.to_datetime(df['Datetime']).dt.time
+  df['Symbol']=symbol
+  df=df[[symbol,'Date','Datetime','Open','High','Low','Close','Volume']]
+  
+  df=df.round(2)
+  return df.tail(1)
+def sub_loop(interval=5):
+  df=pd.Dataframe()
+  for symbol in ['^NSEI','^NSEBANK','^BSESN']:
+    yf_data=get_yf_data(symbol,str(interval)+"m")
+    df=pd.concat([df,yf_data])
+  return df
 def run():
   for i in range(100):
-    df=yf.Ticker("^NSEI").history(interval="5m",period=str(2)+"d")
-    df['Datetime'] = df.index
-    df['Datetime']=df['Datetime'].dt.tz_localize(None)
-    df.index=df['Datetime']
-    df=df[['Datetime','Open','High','Low','Close','Volume']]
-    df['Date']=df['Datetime'].dt.strftime('%m/%d/%y')
-    df['Datetime'] = pd.to_datetime(df['Datetime']).dt.time
-    df=df[['Date','Datetime','Open','High','Low','Close','Volume']]
-    df=df.round(2)
+    df=sub_loop(5)
     order_datatable.dataframe(df,hide_index=True)
     order_book_updated.text(f"Orderbook : {datetime.datetime.now(tz=gettz('Asia/Kolkata')).time().replace(microsecond=0)}")
     time.sleep(60)
